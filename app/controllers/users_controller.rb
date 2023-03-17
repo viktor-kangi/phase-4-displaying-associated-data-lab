69656324a847fc158403db6c2502c4b3911d6e18
victor <victor.buthia@student.moringaschool.com>
2023-03-17 12:18:57 +0300

    completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+    def show 
+        # show the current user
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end 
+
 end
